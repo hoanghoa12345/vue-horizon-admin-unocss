8ed6d49edd3691ac782a98e2c7819b8023726c49
Hoang Hoa <44923253+hoanghoa12345@users.noreply.github.com>
2026-08-07 14:27:30 +0700

    Update pnpm workspace

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
-  css: ['~/assets/css/main.css']
+  modules: ['@pinia/nuxt'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
